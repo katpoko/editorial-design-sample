fada8b76c6664c21ee3b5da34da4afd9e85ecab3
Kat Pokorny <145707652+katpoko@users.noreply.github.com>
2023-11-28 19:31:38 +0000

    beta version, added gsap plug in

--- README.md
+++ README.md
@@ -8,3 +8,7 @@ Stuff about coffee. Coffee is life.
 * Github Copilot
 * [Coffee](https://en.wikipedia.org/wiki/Coffee)
 * [favicon](https://favicon.io/)
+
+<!-- q: why am i so weird? -->
+
+<!-- q: why do dogs bark? -->
--- script.js
+++ script.js
@@ -1,1 +1,60 @@
 console.log( '😀👍🍎' );
+
+// GSAP library
+// https://unpkg.co/gsap@3/dist/gsap.min.js
+
+// ScrollTrigger plugin
+// https://unpkg.com/gsap@3/dist/ScrollTrigger.min.js
+
+// Install & Activate Plugin
+gsap.registerPlugin(ScrollTrigger);
+
+// defaults
+ScrollTrigger.defaults({
+  toggleActions: "restart pause resume pause"
+});
+
+// Panel One
+gsap.to(".one", {
+  scrollTrigger: "one",
+  backgroundColor: "red",
+  duration: 2
+});
+
+// Panel Two
+gsap.to(".two", {
+  scrollTrigger: "two"
+});
+
+// Panel Three
+gsap.to(".three p", {
+  scrollTrigger: "three",
+  letterSpacing: 0,
+  duration: 5
+});
+
+// Panel Four
+gsap.to(".four", {
+  scrollTrigger: "four"
+});
+
+// Panel Five
+gsap.to(".five", {
+  scrollTrigger: "five"
+});
+
+// Panel Six
+gsap.to(".six", {
+  scrollTrigger: "six",
+  translateX: 0,
+  duration: 2
+});
+
+// Panel One
+gsap.to(".one h1", {
+  scrollTrigger: "one",
+  color: "brown",
+  duration: 3,
+  rotate: 0,
+  opacity: 1
+});
